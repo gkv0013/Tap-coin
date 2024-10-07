3d061196fb992038846fa068751809a47c532724
gkv0013 <66899128+gkv0013@users.noreply.github.com>
2024-10-08 01:43:26 +0530

    goals

--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -1,10 +1,11 @@
 import { CommonService } from '../common.service';
 import { CollectService } from '../../core/services/collect.service';
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute, RouterModule } from '@angular/router'; // Import ActivatedRoute
 import { TelegramWebappService } from '@zakarliuka/ng-telegram-webapp';
 import { Goal } from '../../core/interface/user';
+import { DialogService } from '../../core/components/dialog/dialog.service';
 declare var Telegram: any;
 @Component({
   selector: 'app-goals',
@@ -16,14 +17,19 @@ declare var Telegram: any;
 export class GoalComponent implements OnInit {
   public commonService = inject(CommonService);
   public router = inject(Router);
-  private route = inject(ActivatedRoute); // Inject ActivatedRoute
+  private route = inject(ActivatedRoute);
   private telegramServices = inject(TelegramWebappService);
+  private dialogService = inject(DialogService);
+  @ViewChild('modalyoutube') modalyoutube!: TemplateRef<any>;
   public currentGoalNo: string | null = null; // Store the user ID
-  public goalData: any; // Variable to hold a single object
+  public selectedGoalData: any; // Variable to hold a single object
+  public selectedYoutubeVideo:any;
   public goalsData: Goal[] = [];
+  public youtubeVideos: any[] = [];
   isOverviewSelected: boolean = true;
-
+  activeTab: string = 'overview';
   selectTab(tab: string) {
+    this.activeTab=tab;
     if (tab === 'overview') {
       this.isOverviewSelected = true;
     } else {
@@ -34,9 +40,10 @@ export class GoalComponent implements OnInit {
   ngOnInit(): void {
     this.goalsData=this.commonService.getGoalData() ?? [];
     this.currentGoalNo = this.route.snapshot.paramMap.get('id');
-    this.goalData = this.goalsData.find(
+    this.selectedGoalData = this.goalsData.find(
       (goal) => goal?.goalno === Number(this.currentGoalNo)
     );
+    this.parseYoutubeData();
     this.enableBackButton();
   }
   enableBackButton() {
@@ -61,6 +68,30 @@ export class GoalComponent implements OnInit {
     const url = moreInfoObject.url;
     this.router.navigate(['/more-info'], {
       queryParams: { id: goalData.goalNo, url: url },
+    });
+  }
+  closeModal() {
+    this.dialogService.closeDialog();
+    this.selectedYoutubeVideo=[]
+  }
+  parseYoutubeData(): void {
+    if (this.selectedGoalData.youtube) {
+      try {
+        this.youtubeVideos = JSON.parse(this.selectedGoalData.youtube);
+      } catch (error) {
+        console.error('Error parsing youtube data:', error);
+      }
+    }
+  }
+  modalYoutube(modalYoutube:any) {
+    this.selectedYoutubeVideo=modalYoutube;
+    this.dialogService.openDialog({
+      title: '',
+      message: '',
+      type: '',
+      customClass: '',
+      customTemplate: this.modalyoutube,
+      onConfirm: () => {},
     });
   }
 }
