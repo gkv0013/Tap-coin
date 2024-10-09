11cafc209a5ad7c2d90025aeb5b7f5e3f0e044fe
KRISHNADAS-KD <mrkrishnadaspv@gmail.com>
2024-10-09 22:52:50 +0400

    update

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     telegramBotToken: '7092587891:AAHZqlRZsNqqsMEs5F6dhzstQL___NSEV3A',
-      apiUrl: 'https://your-production-url/api'
+    apiUrl: 'https://nilaapi20240917193848.azurewebsites.net/api'
   };
   
