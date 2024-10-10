12fd6e46e6c33cc03193df6899149cd8123f4150
KD <mrkrishnadaspv@gmail.com>
2024-10-10 14:29:13 +0400

    Update environment.prod.ts
    url chnage

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     telegramBotToken: '7092587891:AAHZqlRZsNqqsMEs5F6dhzstQL___NSEV3A',
-    apiUrl: 'https://nilaapi20240917193848.azurewebsites.net/api'
+    apiUrl: 'http://51.20.75.24:5050/api'
   };
   
